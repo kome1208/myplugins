f65f8847295ff145e8ae28ba57576e75c25f3abc
kome1208 <kome1208@outlook.jp>
2023-12-04 01:34:38 +0900

    y

---
+++ plugins/theme/src/index.ts
@@ -0,0 +1,17 @@
+import { findByProps } from "@vendetta/metro";
+import { instead } from "@vendetta/patcher";
+
+const { canUseClientThemes } = findByProps("canUseClientThemes");
+
+const patches = [];
+
+export default {
+    onLoad: () => {
+        patches.push(
+            instead("canUseClientThemes", canUseClientThemes, () => true)
+        );
+    },
+    onUnload: () => {
+        patches.forEach(p => p.unpatch());
+    }
+}
